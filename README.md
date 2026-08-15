770d785181481e80a9f77d9b6677b13166f7d22f
salvadorrodriguez <sal.rodloy@gmail.com>
2026-08-15 17:38:58 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Video Raccoon
+
+## Build
+```shell
+./gradflew build
+```
---
+++ src/test/kotlin/com/videoraccoon/VideoRaccoonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.videoraccoon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VideoRaccoonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
